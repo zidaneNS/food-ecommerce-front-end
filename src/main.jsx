c35a74d21427bf9c73a0ffc0705434b1055238db
unknown <zidane.nur004@gmail.com>
2024-11-24 11:07:06 +0700

    predeploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/authContext.jsx'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 import './index.css'
 import App from './App.jsx'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
